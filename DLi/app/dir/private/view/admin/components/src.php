e7f2e5b788bea0720f38c746cb6f3a0d4635dda7
Thy Antonio <58723692+thyronea@users.noreply.github.com>
2024-03-06 13:52:02 -0800

    Add files via upload

---
+++ DLi/app/dir/private/view/admin/components/src.php
@@ -0,0 +1,14 @@
+<!-- Header Links -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> <!-- Google Chart Library -->
+<script src="https://kit.fontawesome.com/e134814292.js"></script> <!-- Fontawesome Library -->
+
+
+<!-- Footer Links -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.js" ></script>
