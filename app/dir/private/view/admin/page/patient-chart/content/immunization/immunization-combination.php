0e1a1dad830aa37ddf8e7754c77cafdb5a9ba367
thyronea <thyrone.antonio@gmail.com>
2024-05-29 09:46:47 -0700

    Immunization History - Added visual

--- app/dir/private/view/admin/page/patient-chart/content/immunization/immunization-combination.php
+++ app/dir/private/view/admin/page/patient-chart/content/immunization/immunization-combination.php
@@ -16,21 +16,33 @@
                 </div>
             </div>
             <div class="mb-3">
+                <p>Rotavirus - 2 Dose Series</p>
+                <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="height: 25px">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$rota_count;?>%"><?=$rota_count;?>%</div>
+                </div>
+            </div>
+            <div class="mb-3">
                 <p>DTaP - 4 Dose Series</p>
                 <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="height: 25px">
                     <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$dtap_count;?>%"><?=$dtap_count;?>%</div>
                 </div>
             </div>
             <div class="mb-3">
-                <p>IPV - 3 Dose Series</p>
+                <p>Hib - 4 Dose Series</p>
                 <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="height: 25px">
-                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$ipv_count;?>%"><?=$ipv_count;?>%</div>
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$hib_count;?>%"><?=$hib_count;?>%</div>
                 </div>
             </div>
             <div class="mb-3">
-                <p>Rotavirus - 2 Dose Series</p>
+                <p>PCV - 4 Dose Series</p>
                 <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="height: 25px">
-                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$rota_count;?>%"><?=$rota_count;?>%</div>
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$pcv_count;?>%"><?=$pcv_count;?>%</div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <p>IPV - 3 Dose Series</p>
+                <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="height: 25px">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$ipv_count;?>%"><?=$ipv_count;?>%</div>
                 </div>
             </div>
         </div>
--- app/dir/private/view/admin/page/patient-chart/content/immunization/immunization-history.php
+++ app/dir/private/view/admin/page/patient-chart/content/immunization/immunization-history.php
@@ -1,12 +1,48 @@
 <div class="tab-pane fade show active" id="history" role="tabpanel" tabindex="0">
-    <div class="card mt-2">
-        <div class="card-body">
-            <h5>History</h5>
-            <table class="table table-borderless table-sm table-hover text-nowrap">
-                <tbody align="center" style="text-align: left">
-                                       
-                </tbody>
-            </table>
+    <div class="d-flex align-items-start row">
+        <div class="row col-md-10 mt-2 mb-3">
+            <div class="mb-3">
+                <p>RSV - 1 Dose Series</p>
+                <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="height: 25px">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$rsv_count;?>%"><?=$rsv_count;?>%</div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <p>Hepatitis B - 3 Dose Series</p>
+                <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="height: 25px">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$hepB_count;?>%"><?=$hepB_count;?>%</div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <p>Rotavirus - 2 Dose Series</p>
+                <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="height: 25px">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$rota_count;?>%"><?=$rota_count;?>%</div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <p>DTaP - 4 Dose Series</p>
+                <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="height: 25px">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$dtap_count;?>%"><?=$dtap_count;?>%</div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <p>Hib - 4 Dose Series</p>
+                <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="height: 25px">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$hib_count;?>%"><?=$hib_count;?>%</div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <p>PCV - 4 Dose Series</p>
+                <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="height: 25px">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$pcv_count;?>%"><?=$pcv_count;?>%</div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <p>IPV - 3 Dose Series</p>
+                <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="height: 25px">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: <?=$ipv_count;?>%"><?=$ipv_count;?>%</div>
+                </div>
+            </div>
         </div>
-    </div>
+    </div> 
 </div>
