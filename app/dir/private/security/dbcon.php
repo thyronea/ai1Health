370ac13bb4435841753525a0e3998063776bdfa1
thyronea <thyrone.antonio@gmail.com>
2024-07-09 16:41:14 -0700

    db update

--- app/dir/private/security/dbcon.php
+++ app/dir/private/security/dbcon.php
@@ -1,9 +1,9 @@
 <?php
 
 // Create connection
-  $servername = "";
+  $servername = "Ai1 System";
   $username = "aqfusshsmb";
-  $password = "C4AB02C04ADCACFEDB1362859F03B4E03EE59F36";
+  $password = "C4AB02C04ADCACFEDB1362859F03B4E03EE59F36!";
   $database = "aqfusshsmb";
 
   $con = mysqli_connect($servername, $username, $password, $database);
