3ead5f36a9008b749426256d60dc6b507c7524d4
thyronea <thyrone.antonio@gmail.com>
2024-07-09 21:07:44 -0700

    updated links

--- app/dir/private/view/admin/content/logged_out.php
+++ app/dir/private/view/admin/content/logged_out.php
@@ -4,7 +4,7 @@
     <div class="container mt-5">
       <div class="row mt-5">
         <div class="col-md-12 mt-5">
-          <p class="mt-5" align="center">For security, please <a class="text-decoration-none" href="/public/page/access/sign-in.php">log in</a> or <a class="text-decoration-none" href="/public/page/access/register.php">register</a></p>
+          <p class="mt-5" align="center">For security, please <a class="text-decoration-none" href="/public/page/access/sign-in.php">log in</a> or <a class="text-decoration-none" href="/public/page/access/admin-registration.php">register</a></p>
         </div>
       </div>
     </div>
