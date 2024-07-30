ca7718b9aa1dd27b8e39cafc1efc0200d6ee2403
thyronea <thyrone.antonio@gmail.com>
2024-07-29 19:56:38 -0700

    quick update

--- app/dir/private/access/user-registration.php
+++ app/dir/private/access/user-registration.php
@@ -69,8 +69,8 @@ if(isset($_POST['register_user'])) {
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
 
-    $mail->Username = "thyrone.antonio@gmail.com";
-    $mail->Password = "mhopftvkjlemevgn";
+    $mail->Username = "donotreply@ai1system.net";
+    $mail->Password = "awxo vbxo hvix pitc";
 
     $mail->setFrom($email);
     $mail->addAddress($email);
