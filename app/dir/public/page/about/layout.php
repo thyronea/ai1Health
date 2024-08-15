1e814da696faf174ff6ec4ad2086cbf415d6319d
thyronea <thyrone.antonio@gmail.com>
2024-08-15 00:09:59 -0700

    DB fix

--- app/dir/public/page/about/layout.php
+++ app/dir/public/page/about/layout.php
@@ -1,31 +1,31 @@
 <div class="container mt-5">
     <div class="col-md-12" align="center">
         
-        <div class="col-md-8 d-flex mb-3 card border shadow" id="systems">
+        <div class="col-md-8 d-flex mb-3 card border shadow" id="">
             <div class="card-body" style="text-align: start">
                 <img src="../../../private/view/image/background/ai1-data.png" alt="">
             </div>
         </div>
         
-        <div class="col-md-8 d-flex mb-3 card border shadow" id="systems">
+        <div class="col-md-8 d-flex mb-3 card border shadow" id="">
             <div class="card-body" style="text-align: start">
                 <img src="../../../private/view/image/background/ai1-customer.png" alt="">
             </div>
         </div>
 
-        <div class="col-md-8 d-flex mb-3 card border shadow" id="systems">
+        <div class="col-md-8 d-flex mb-3 card border shadow" id="">
             <div class="card-body" style="text-align: start">
                 <img src="../../../private/view/image/background/ai1-calendar.png" alt="">
             </div>
         </div>
 
-        <div class="col-md-8 d-flex mb-3 card border shadow" id="systems">
+        <div class="col-md-8 d-flex mb-3 card border shadow" id="">
             <div class="card-body" style="text-align: start">
                 <img src="../../../private/view/image/background/ai1-folder.png" alt="">
             </div>
         </div>
 
-        <div class="col-md-8 d-flex mb-3 card border shadow" id="systems">
+        <div class="col-md-8 d-flex mb-3 card border shadow" id="">
             <div class="card-body" style="text-align: start">
                 <img src="../../../private/view/image/background/ai1-chat.png" alt="">
             </div>
