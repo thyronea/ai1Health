2ddb22f6c7d2cdc725e1c2e70cefd94e0c22c629
thyronea <thyrone.antonio@gmail.com>
2024-08-15 19:44:57 -0700

    final navbar edit

--- app/dir/public/page/access/admin-registration.php
+++ app/dir/public/page/access/admin-registration.php
@@ -3,7 +3,7 @@ $page_title = 'Registration';
 require_once('../../../private/initialize.php');
 include(PRIVATE_ACCESS_PATH . '/admin-registration.php');
 include(PRIVATE_COMPONENTS_PATH . '/header.php');
-include(PRIVATE_COMPONENTS_PATH . '/navbar2.php');
+include(PRIVATE_COMPONENTS_PATH . '/health-navbar.php');
 include(PUBLIC_FORMS_PATH . '/admin-registration-form.php');
 include(PRIVATE_COMPONENTS_PATH . '/footer.php');
 ?>
