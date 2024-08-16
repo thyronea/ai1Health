86f90c0c2e7172caed21c819da5690612bc460da
thyronea <thyrone.antonio@gmail.com>
2024-08-15 19:42:38 -0700

    Quick edit

--- app/dir/public/page/access/jb-sign-in.php
+++ app/dir/public/page/access/jb-sign-in.php
@@ -3,7 +3,7 @@ $page_title = 'Job Board Sign In';
 require_once('../../../private/initialize.php');
 include(PRIVATE_SECURITY_PATH . '/verification-code-process.php');
 include(PRIVATE_COMPONENTS_PATH . '/header.php');
-include(PRIVATE_COMPONENTS_PATH . '/navbar2.php');
+include(PRIVATE_COMPONENTS_PATH . '/jb-navbar.php');
 include(PUBLIC_FORMS_PATH . '/jb-sign-in-form.php');
 include(PRIVATE_COMPONENTS_PATH . '/footer.php');
 ?>
--- app/dir/public/page/access/sign-in.php
+++ app/dir/public/page/access/sign-in.php
@@ -3,7 +3,7 @@ $page_title = 'Sign In';
 require_once('../../../private/initialize.php');
 include(PRIVATE_SECURITY_PATH . '/verification-code-process.php');
 include(PRIVATE_COMPONENTS_PATH . '/header.php');
-include(PRIVATE_COMPONENTS_PATH . '/navbar2.php');
+include(PRIVATE_COMPONENTS_PATH . '/health-navbar.php');
 include(PUBLIC_FORMS_PATH . '/sign-in-form.php');
 include(PRIVATE_COMPONENTS_PATH . '/footer.php');
 ?>
