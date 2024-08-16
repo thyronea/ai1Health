4bb8b1c0189491c0eacd9535e28ec9619817b1b6
thyronea <thyrone.antonio@gmail.com>
2024-08-15 19:44:05 -0700

    Navbar quick edit

--- app/dir/public/page/access/user-registration.php
+++ app/dir/public/page/access/user-registration.php
@@ -3,7 +3,7 @@ $page_title = 'Registration';
 require_once('../../../private/initialize.php');
 include(PRIVATE_ACCESS_PATH . '/user-registration.php');
 include(PRIVATE_COMPONENTS_PATH . '/header.php');
-include(PRIVATE_COMPONENTS_PATH . '/navbar2.php');
+include(PRIVATE_COMPONENTS_PATH . '/health-navbar.php');
 include(PUBLIC_FORMS_PATH . '/user-registration-form.php');
 include(PRIVATE_COMPONENTS_PATH . '/footer.php');
 ?>
