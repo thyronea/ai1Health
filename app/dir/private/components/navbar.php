71b1d4851981d6fa2677fc5edeb0c1c2d6c49aa3
thyronea <thyrone.antonio@gmail.com>
2024-08-16 12:27:25 -0700

    Navbar update

--- app/dir/private/components/navbar.php
+++ app/dir/private/components/navbar.php
@@ -24,9 +24,6 @@
 <nav class="navbar navbar-expand-lg bg-body sticky-top">
   <div class="container">
     <a class="navbar-brand" href="index.php">AI1 System</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
   </div>
 </nav>
 
