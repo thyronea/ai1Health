aa3ceec52f64b5a3f93aca56b0fa9c28374d6242
thyronea <thyrone.antonio@gmail.com>
2024-10-18 16:54:59 -0700

    removed additional files

--- app/dir/public/page/access/jb-sign-in.php
+++
@@ -1,9 +0,0 @@
-<?php
-$page_title = 'Job Board Sign In';
-require_once('../../../private/initialize.php');
-include(PRIVATE_SECURITY_PATH . '/verification-code-process.php');
-include(PRIVATE_COMPONENTS_PATH . '/header.php');
-include(PRIVATE_COMPONENTS_PATH . '/jb-navbar.php');
-include(PUBLIC_FORMS_PATH . '/jb-sign-in-form.php');
-include(PRIVATE_COMPONENTS_PATH . '/footer.php');
-?>
