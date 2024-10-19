8b921f8d7bc86200e064a3cd2e42e5ae72f4586f
thyronea <thyrone.antonio@gmail.com>
2024-10-18 18:12:04 -0700

    removed files

--- app/dir/forgot-password/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$page_title = 'Forgot Password';
-require_once('../private/initialize.php');
-include(PRIVATE_ACCESS_PATH . '/login-process.php');
-include(PRIVATE_COMPONENTS_PATH . '/header.php');
-include('..//healthcare/navbar.php');
-include(PUBLIC_FORMS_PATH . '/forgot-pw-form.php');
-include(PRIVATE_COMPONENTS_PATH . '/footer.php');
-?>
